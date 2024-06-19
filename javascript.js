1f53337077358e6a09e7da28157de1313587ed7e
Ethan Ryan <144191914+JungitRyan@users.noreply.github.com>
2024-06-19 14:58:24 -0400

    Create getHumanChoice function

--- javascript.js
+++ javascript.js
@@ -1,10 +1,19 @@
 function getComputerChoice() {
-  let val = Math.random;
+  let val = Math.random();
   if (val < 0.33) {
     return "Rock";
-  } else if (0.33 < val < 0.66) {
+  } else if (val < 0.66) {
     return "Paper";
   } else {
     return "Scissors";
   }
 }
+
+function getHumanChoice() {
+  let choice = prompt("Choose rock, paper, or scissors!");
+  return choice.toLowerCase();
+}
+
+// Main method
+getComputerChoice();
+getHumanChoice();
